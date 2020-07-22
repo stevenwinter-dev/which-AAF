484f7777f4d2dcf275bb3586429db5287a4e3bb0
Steven Winter <stevenwinter.me@gmail.com>
2020-07-22 15:57:26 -0500

    Restructured file layout. Add Team.js, changed Nfl.js. Moved imports from app.js to those new files, creating a much cleaner app.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import React, { useState } from 'react';
 
-import ravens from './images/ravens.png';
-import bills from './images/bills.png';
-import bengals from './images/bengals.png';
-import browns from './images/browns.png';
-import broncos from './images/broncos.png';
-import texans from './images/texans.png';
-import colts from './images/colts.png';
-import jaguars from './images/jaguars.png';
-import chiefs from './images/chiefs.png';
-import chargers from './images/chargers.png';
-import dolphins from './images/dolphins.png';
-import pats from './images/pats.png';
-import jets from './images/jets.png';
-import raiders from './images/raiders.png';
-import steelers from './images/steelers.png';
-import titans from './images/titans.png';
-
-import cards from './images/cards.png';
-import falcons from './images/falcons.png';
-import panthers from './images/panthers.png';
-import bears from './images/bears.png';
-import cowboys from './images/cowboys.png';
-import lions from './images/lions.png';
-import packers from './images/packers.png';
-import rams from './images/rams.png';
-import vikings from './images/vikings.png';
-import saints from './images/saints.png';
-import giants from './images/giants.png';
-import eagles from './images/eagles.png';
-import niners from './images/niners.png';
-import seahawks from './images/seahawks.png';
-import buccaneers from './images/buccaneers.png';
-import skins from './images/skins.png';
-
 import './App.css';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Aaf from './component/aaf';
-import Nfl from './component/nfl';
+import NFL from './component/Nfl';
 
 function App(){
   const [team, setTeam] = useState('');
@@ -58,46 +24,7 @@ function App(){
         <Header />
         <div className="site-content">
         <p className="instructions">Select your favorite NFL team</p>
-        <div className="conference afc">
-        <div className="afc">
-          <Nfl onClick={onClick} team={"Ravens"} img={ravens} />
-          <Nfl onClick={onClick} team={"Bills"} img={bills}  />
-          <Nfl onClick={onClick} team={"Bengals"} img={bengals}  />
-          <Nfl onClick={onClick} team={"Browns"} img={browns}  />
-          <Nfl onClick={onClick} team={"Broncos"} img={broncos}  />
-          <Nfl onClick={onClick} team={"Texans"} img={texans}  />
-          <Nfl onClick={onClick} team={"Colts"} img={colts}  />
-          <Nfl onClick={onClick} team={"Jaguars"} img={jaguars}  />
-          <Nfl onClick={onClick} team={"Chiefs"} img={chiefs}  />
-          <Nfl onClick={onClick} team={"Chargers"} img={chargers}  />
-          <Nfl onClick={onClick} team={"Dolphins"} img={dolphins}  />
-          <Nfl onClick={onClick} team={"Patriots"} img={pats}  />
-          <Nfl onClick={onClick} team={"Jets"} img={jets}  />
-          <Nfl onClick={onClick} team={"Raiders"} img={raiders}  />
-          <Nfl onClick={onClick} team={"Steelers"} img={steelers}  />
-          <Nfl onClick={onClick} team={"Titans"} img={titans}  />
-          </div>
-          <br />
-          <div className="nfc">
-          <Nfl onClick={onClick} team={"Cardinals"} img={cards}  />
-          <Nfl onClick={onClick} team={"Falcons"} img={falcons}  />
-          <Nfl onClick={onClick} team={"Panthers"} img={panthers}  />
-          <Nfl onClick={onClick} team={"Bears"} img={bears}  />
-          <Nfl onClick={onClick} team={"Cowboys"} img={cowboys}  />
-          <Nfl onClick={onClick} team={"Lions"} img={lions}  />
-          <Nfl onClick={onClick} team={"Packers"} img={packers}  />
-          <Nfl onClick={onClick} team={"Rams"} img={rams}  />
-          <Nfl onClick={onClick} team={"Vikings"} img={vikings}  />
-          <Nfl onClick={onClick} team={"Saints"} img={saints}  />
-          <Nfl onClick={onClick} team={"Giants"} img={giants}  />
-          <Nfl onClick={onClick} team={"Eagles"} img={eagles}  />
-          <Nfl onClick={onClick} team={"49ers"} img={niners}  />
-          <Nfl onClick={onClick} team={"Seahawks"} img={seahawks}  />
-          <Nfl onClick={onClick} team={"Buccaneers"} img={buccaneers}  />
-          <Nfl onClick={onClick} team={"Skins"} img={skins}  />        
-          </div>
-        </div>
-
+        <NFL onClick={onClick} />
         {selected ? 
           <Aaf team={team} img={img} onClick={onClick} /> 
           : null }
--- src/component/nfl.js
+++ src/component/nfl.js
@@ -1,11 +1,82 @@
 import React from 'react';
+import Team from './Team';
 
-const Nfl = ( props ) => {
+import ravens from '../images/ravens.png';
+import bills from '../images/bills.png';
+import bengals from '../images/bengals.png';
+import browns from '../images/browns.png';
+import broncos from '../images/broncos.png';
+import texans from '../images/texans.png';
+import colts from '../images/colts.png';
+import jaguars from '../images/jaguars.png';
+import chiefs from '../images/chiefs.png';
+import chargers from '../images/chargers.png';
+import dolphins from '../images/dolphins.png';
+import pats from '../images/pats.png';
+import jets from '../images/jets.png';
+import raiders from '../images/raiders.png';
+import steelers from '../images/steelers.png';
+import titans from '../images/titans.png';
+
+import cards from '../images/cards.png';
+import falcons from '../images/falcons.png';
+import panthers from '../images/panthers.png';
+import bears from '../images/bears.png';
+import cowboys from '../images/cowboys.png';
+import lions from '../images/lions.png';
+import packers from '../images/packers.png';
+import rams from '../images/rams.png';
+import vikings from '../images/vikings.png';
+import saints from '../images/saints.png';
+import giants from '../images/giants.png';
+import eagles from '../images/eagles.png';
+import niners from '../images/niners.png';
+import seahawks from '../images/seahawks.png';
+import buccaneers from '../images/buccaneers.png';
+import skins from '../images/skins.png';
+
+function Nfl(props) {
     return(
-        <span>
-            <img src={props.img} className="nfl-team-logo" alt={props.team} onClick={props.onClick}/>
-        </span>
+        <div className="conference afc">
+            <div className="afc">
+                <Team onClick={props.onClick} team={"Ravens"} img={ravens} />
+                <Team onClick={props.onClick} team={"Bills"} img={bills}  />
+                <Team onClick={props.onClick} team={"Bengals"} img={bengals}  />
+                <Team onClick={props.onClick} team={"Browns"} img={browns}  />
+                <Team onClick={props.onClick} team={"Broncos"} img={broncos}  />
+                <Team onClick={props.onClick} team={"Texans"} img={texans}  />
+                <Team onClick={props.onClick} team={"Colts"} img={colts}  />
+                <Team onClick={props.onClick} team={"Jaguars"} img={jaguars}  />
+                <Team onClick={props.onClick} team={"Chiefs"} img={chiefs}  />
+                <Team onClick={props.onClick} team={"Chargers"} img={chargers}  />
+                <Team onClick={props.onClick} team={"Dolphins"} img={dolphins}  />
+                <Team onClick={props.onClick} team={"Patriots"} img={pats}  />
+                <Team onClick={props.onClick} team={"Jets"} img={jets}  />
+                <Team onClick={props.onClick} team={"Raiders"} img={raiders}  />
+                <Team onClick={props.onClick} team={"Steelers"} img={steelers}  />
+                <Team onClick={props.onClick} team={"Titans"} img={titans}  />
+                </div>
+                <br />
+                <div className="nfc">
+                <Team onClick={props.onClick} team={"Cardinals"} img={cards}  />
+                <Team onClick={props.onClick} team={"Falcons"} img={falcons}  />
+                <Team onClick={props.onClick} team={"Panthers"} img={panthers}  />
+                <Team onClick={props.onClick} team={"Bears"} img={bears}  />
+                <Team onClick={props.onClick} team={"Cowboys"} img={cowboys}  />
+                <Team onClick={props.onClick} team={"Lions"} img={lions}  />
+                <Team onClick={props.onClick} team={"Packers"} img={packers}  />
+                <Team onClick={props.onClick} team={"Rams"} img={rams}  />
+                <Team onClick={props.onClick} team={"Vikings"} img={vikings}  />
+                <Team onClick={props.onClick} team={"Saints"} img={saints}  />
+                <Team onClick={props.onClick} team={"Giants"} img={giants}  />
+                <Team onClick={props.onClick} team={"Eagles"} img={eagles}  />
+                <Team onClick={props.onClick} team={"49ers"} img={niners}  />
+                <Team onClick={props.onClick} team={"Seahawks"} img={seahawks}  />
+                <Team onClick={props.onClick} team={"Buccaneers"} img={buccaneers}  />
+                <Team onClick={props.onClick} team={"Skins"} img={skins}  />        
+            </div>
+        </div>
     )
-};
+}
 
 export default Nfl;
